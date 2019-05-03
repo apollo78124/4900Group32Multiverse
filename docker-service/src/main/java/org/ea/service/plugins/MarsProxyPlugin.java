49e0943697adb1fbd90b2f901725eaf9ee127f6b
apollo78124 <98apollo@naver.com>
2019-05-03 11:30:19 -0700

    broken maven project to be fixed

---
+++ docker-service/src/main/java/org/ea/service/plugins/MarsProxyPlugin.java
@@ -0,0 +1,26 @@
+package multiverse.mars.plugins;
+
+import multiverse.server.plugins.*;
+import multiverse.server.events.*;
+import multiverse.server.network.*;
+
+/**
+ * handles client traffic to the rest of the servers
+ */
+public class MarsProxyPlugin extends ProxyPlugin {
+    
+    /**
+     * process login message from the client.
+     */
+    protected boolean processLogin(
+	ClientConnection con, AuthorizedLoginEvent loginEvent) {
+
+        if (! super.processLogin(con, loginEvent))
+	    return false;
+        
+        // Example: get the inventory
+
+        return true;
+    }
+
+}
